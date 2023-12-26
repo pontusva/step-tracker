57c22dbcb489e6f4d979f543359e5c927ed2666f
Pontus A <pontus.abrahamsson@iths.se>
2023-12-26 18:06:50 +0100

    Update syncSteps query to include date and handle conflicts

--- backend/queries.ts
+++ backend/queries.ts
@@ -3,7 +3,10 @@ export default {
     signUp: 'INSERT INTO users (uid, name) VALUES ($1, $2) RETURNING *',
   },
   sync: {
-    syncSteps:
-      'INSERT INTO steps (uid, step_count_day) VALUES ($1, $2) RETURNING *',
+    syncSteps: `INSERT INTO steps (uid, date, step_count_day) 
+      VALUES ($1, CURRENT_DATE, $2) 
+      ON CONFLICT (uid, date) 
+      DO UPDATE SET step_count_day = EXCLUDED.step_count_day 
+      RETURNING *`,
   },
 };
