de45192899fe3581e895f61230feccb7a6a17921
Pontus A <pontus.abrahamsson@iths.se>
2023-12-26 11:57:02 +0100

    Add image background and styling to HomeScreen, and improve user experience in SettingsScreen

--- app/screens/pages/HomeScreen.tsx
+++ app/screens/pages/HomeScreen.tsx
@@ -1,13 +1,43 @@
-import { Button, Text } from 'react-native';
+import { Button, ImageBackground, StyleSheet, Text } from 'react-native';
 import { getAuth } from 'firebase/auth';
+import { useEffect } from 'react';
 export const HomeScreen = ({ navigation }) => {
   const auth = getAuth().currentUser;
   console.log(auth.displayName);
 
+  useEffect(() => {
+    if (!auth) {
+      navigation.navigate('SignIn');
+    }
+  }, []);
   return (
     <>
-      <Button title="user" onPress={() => console.log(auth)} />
-      <Text>Welcome, {auth.displayName}</Text>
+      <ImageBackground
+        source={require('../../assets/homescreen.jpg')}
+        resizeMode="cover"
+        style={styles.image}>
+        {/* <Button title="user" onPress={() => console.log(auth.displayName)} /> */}
+        <Text style={styles.text}>Hej {auth.displayName}</Text>
+      </ImageBackground>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'flex-start',
+  },
+  text: {
+    marginTop: 100,
+    color: 'white',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
+  },
+});
--- app/screens/pages/SettingsScreen.tsx
+++ app/screens/pages/SettingsScreen.tsx
@@ -1,5 +1,14 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, TextInput, Button } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Button,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import {
   getAuth,
   updateProfile,
@@ -24,6 +33,8 @@ export const SettingsScreen = () => {
         alert('Name updated successfully');
       } catch (error) {
         console.error(error);
+      } finally {
+        setName('');
       }
     }
   };
@@ -38,6 +49,8 @@ export const SettingsScreen = () => {
         alert('Password updated successfully');
       } catch (error) {
         console.error(error);
+      } finally {
+        setPassword('');
       }
     }
   };
@@ -65,32 +78,38 @@ export const SettingsScreen = () => {
   };
 
   return (
-    <View>
-      <TextInput
-        style={styles.input}
-        value={name}
-        onChangeText={setName}
-        placeholder="New Name"
-      />
-      <Button title="Update Name" onPress={updateName} />
-      <TextInput
-        style={styles.input}
-        value={password}
-        onChangeText={setPassword}
-        placeholder="New Password"
-        secureTextEntry
-      />
-      <Button title="Update Password" onPress={updatePasswordFunc} />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.container}>
+        <View>
+          <TextInput
+            style={styles.input}
+            value={name}
+            onChangeText={setName}
+            placeholder="New Name"
+          />
+          <Button title="Update Name" onPress={updateName} />
+          <TextInput
+            style={styles.input}
+            value={password}
+            onChangeText={setPassword}
+            placeholder="New Password"
+            secureTextEntry
+          />
+          <Button title="Update Password" onPress={updatePasswordFunc} />
 
-      <TextInput
-        style={styles.input}
-        value={passwordDelete}
-        onChangeText={setPasswordDelete}
-        placeholder="Current Password"
-        secureTextEntry
-      />
-      <Button title="Delete Account" onPress={deleteAccount} />
-    </View>
+          <TextInput
+            style={styles.input}
+            value={passwordDelete}
+            onChangeText={setPasswordDelete}
+            placeholder="Current Password"
+            secureTextEntry
+          />
+          <Button title="Delete Account" onPress={deleteAccount} />
+        </View>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -99,5 +118,27 @@ const styles = StyleSheet.create({
     height: 40,
     margin: 12,
     borderWidth: 1,
+  },
+  container: {
+    flex: 1,
+  },
+  inner: {
+    padding: 24,
+    flex: 1,
+    justifyContent: 'space-around',
+  },
+  header: {
+    fontSize: 36,
+    marginBottom: 48,
+  },
+  textInput: {
+    height: 40,
+    borderColor: '#000000',
+    borderBottomWidth: 1,
+    marginBottom: 36,
+  },
+  btnContainer: {
+    backgroundColor: 'white',
+    marginTop: 12,
   },
 });
