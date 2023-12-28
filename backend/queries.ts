d61d9e6d6f83d5ea2bf007c4cc8ed07d59cc707c
Pontus A <pontus.abrahamsson@iths.se>
2023-12-28 03:20:57 +0100

    Refactor friendRequest routes

--- backend/queries.ts
+++ backend/queries.ts
@@ -10,34 +10,4 @@ export default {
                   DO UPDATE SET step_count_day = EXCLUDED.step_count_day 
                   RETURNING *`,
   },
-  friendRequest: {
-    sendRequest: `INSERT INTO friendships (user_uid, friend_uid, status, action_user_uid)
-                    VALUES ($1, $2, 'PENDING', $1)
-                    RETURNING *`,
-
-    acceptRequest: `UPDATE friendships
-                      SET status = 'ACCEPTED', action_user_uid = $1
-                      WHERE user_uid = $2 AND friend_uid = $1 AND status = 'PENDING'
-                      RETURNING *`,
-
-    checkFriendship: `SELECT * FROM friendships
-                        WHERE user_uid = $1 AND friend_uid = $2 AND status = 'ACCEPTED'`,
-
-    getAllFriends: `(SELECT f.*, u.name AS friend_name 
-      FROM friendships f
-      JOIN users u ON f.friend_uid = u.uid
-      WHERE f.user_uid = $1 AND f.status = 'ACCEPTED')
-      
-      UNION
-      
-      (SELECT f.*, u.name AS friend_name 
-      FROM friendships f
-      JOIN users u ON f.user_uid = u.uid
-      WHERE f.friend_uid = $1 AND f.status = 'ACCEPTED')`,
-
-    getFriendRequests: `SELECT f.*, u.name AS friend_name 
-    FROM friendships f 
-    JOIN users u ON f.friend_uid = u.uid 
-    WHERE f.user_uid = $1 AND f.status = 'PENDING'`,
-  },
 };
