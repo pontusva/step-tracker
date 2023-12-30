0d7b4999895f1246e66ad8932c3aa409602f124a
Pontus A <pontus.abrahamsson@iths.se>
2023-12-30 11:54:26 +0100

    Add background image and styling to CompareWithFriend screen

--- app/screens/pages/CompareWithFriend.tsx
+++ app/screens/pages/CompareWithFriend.tsx
@@ -1,7 +1,9 @@
-import { View, Text, SafeAreaView, Button } from 'react-native';
+import { View, Text, SafeAreaView, Button, StyleSheet } from 'react-native';
 import { useFriendStore } from '../../zustand/hooks';
 import { useEffect, useState } from 'react';
 import { getAuth } from 'firebase/auth';
+
+import { ImageBackground } from 'react-native';
 
 export default function CompareWithFriend({ navigation }) {
   const [friendData, setFriendData] = useState([]);
@@ -54,48 +56,77 @@ export default function CompareWithFriend({ navigation }) {
     getYourData();
   }, []);
   return (
-    <SafeAreaView>
-      <View>
-        <Button title="Go back" onPress={() => navigation.goBack()} />
-        <View
-          style={{
-            marginTop: 50,
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            alignItems: 'center',
-          }}>
+    <ImageBackground
+      source={require('../../assets/homescreen.jpg')}
+      resizeMode="cover"
+      style={styles.image}>
+      <View style={styles.container}>
+        <SafeAreaView>
           <View>
-            {friendData &&
-              friendData.map((item: any) => {
-                return (
-                  <View key={item.name}>
-                    <Text>{item.name}</Text>
-                    <Text>{item.step_count_day && item.step_count_day}</Text>
-                  </View>
-                );
-              })}
-          </View>
+            <Button title="Go back" onPress={() => navigation.goBack()} />
+            <View
+              style={{
+                marginTop: 50,
+                flexDirection: 'row',
+                justifyContent: 'space-around',
+                alignItems: 'center',
+              }}>
+              <View>
+                {friendData &&
+                  friendData.map((item: any) => {
+                    return (
+                      <View key={item.name}>
+                        <Text style={styles.text}>{item.name}</Text>
+                        <Text style={styles.text}>
+                          {item.step_count_day && item.step_count_day}
+                        </Text>
+                      </View>
+                    );
+                  })}
+              </View>
 
-          <View>
-            {yourData &&
-              yourData.map((item: any) => {
-                return (
-                  <View key={item.name}>
-                    <Text>You</Text>
-                    <Text>{item && item.step_count_day}</Text>
-                  </View>
-                );
-              })}
+              <View>
+                {yourData &&
+                  yourData.map((item: any) => {
+                    return (
+                      <View key={item.name}>
+                        <Text style={styles.text}>You</Text>
+                        <Text style={styles.text}>
+                          {item && item.step_count_day}
+                        </Text>
+                      </View>
+                    );
+                  })}
+              </View>
+            </View>
           </View>
-        </View>
+          <View
+            style={{
+              alignItems: 'center',
+              marginTop: 50,
+            }}>
+            <Text>{winner}</Text>
+          </View>
+        </SafeAreaView>
       </View>
-      <View
-        style={{
-          alignItems: 'center',
-          marginTop: 50,
-        }}>
-        <Text>{winner}</Text>
-      </View>
-    </SafeAreaView>
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000c0',
+  },
+  text: {
+    color: 'white',
+    fontSize: 16,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'flex-start',
+  },
+});
